edb187576a0dba58b0e2292fb94f6731525f47c2
lcarvalhodev <lcarvalho.dev@gmail.com>
2021-03-20 21:31:01 -0300

    fix: fix problem on SignUp routes tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,29 +1,29 @@
 import request from 'supertest'
-import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
-    beforeAll(async () => {
-      await MongoHelper.connect(process.env.MONGO_URL)
-    })
-
-    afterAll(async () => {
-      await MongoHelper.disconnect()
-    })
-
-    beforeEach(async () => {
-      const accountCollection = MongoHelper.getCollection('accounts')
-      await accountCollection.deleteMany({})
-    })
-
     await request(app)
       .post('/api/signup')
       .send({
-        name: 'Leandro',
-        email: 'leandro@gmail.com',
+        name: 'Rodrigo',
+        email: 'rodrigo.manguinho@gmail.com',
         password: '123',
-        password_confirmation: '123'
+        passwordConfirmation: '123'
       })
       .expect(200)
   })
